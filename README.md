308576497f04107c3beccc75399f18e8a4c51e60
briandorado <jblarson2001@gmail.com>
2025-06-19 14:22:50 -0600

    Basic Structure and JSON Files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DND
+
+A Simple Project where we solve all of the world's problems but do not share any of the answers.
---
+++ index.js
@@ -0,0 +1,32 @@
+// import copy
+//     from stats import DEFAULT_STATS
+//
+//
+// class Character:
+// def __init__(self, name: str, character_class: str, race: str,  level: int, stats: dict = None):
+// self.name = name
+// self.level = level
+// self.stats = stats if stats is not None else copy.deepcopy(DEFAULT_STATS) #default stats if not provided
+//
+// def __repr__(self):
+// return f"Character(name={self.name}, level={self.level}, stats={self.stats.hit_points})"
+
+
+class Character {
+    constructor (self, name) {
+        this.self = self
+        this.name = name
+    }
+    get_name () {
+        return this.name
+    }
+
+    set_name (name) {
+        this.name = name
+    }
+
+    static HP = 0;
+}
+
+
+Khufu = new Character('Khufu')
